cdf61d66bcce607b2b6937ec3f60f992c5da5549
Bui Duc Canh <buiduccanh10@gmail.com>
2024-03-22 22:31:15 +0700

    front-end: post create page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('home', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("home", { title: "Express" });
+});
+
+router.get("/postCreate", function (req, res, next) {
+  res.render("postCreate", { title: "postCreate" });
 });
 
 module.exports = router;
