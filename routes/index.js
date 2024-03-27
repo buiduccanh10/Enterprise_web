f56ad5c6f91e16c2bef375070a0178e17c43462b
Bui Duc Canh <buiduccanh10@gmail.com>
2024-03-27 23:36:16 +0700

    feat: post and show newspaper

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,69 @@
 var express = require("express");
 var router = express.Router();
 const { checkStudentSession } = require("../middlewares/auth");
+const multipart = require("connect-multiparty");
+const multipartMiddleware = multipart();
+const fs = require("fs");
+const path = require("path");
+var PostModel = require("../model/post");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("home", { layout: "layout" });
+router.get("/", async function (req, res, next) {
+  const post = await PostModel.find({});
+  res.render("home", {
+    layout: "layout",
+    data: post,
+  });
 });
 
 router.get("/postCreate", checkStudentSession, function (req, res, next) {
   res.render("postCreate", { layout: "layout" });
+});
+
+router.post("/upload", multipartMiddleware, (req, res) => {
+  try {
+    let fileName = Date.now() + req.files.upload.name;
+    fs.readFile(req.files.upload.path, function (err, data) {
+      const newPath = path.join(__dirname, "../public/uploads/", fileName);
+      fs.writeFile(newPath, data, function (err) {
+        if (err) console.log({ err: err });
+        else {
+          console.log(req.files.upload.originalFilename);
+
+          let url = "/uploads/" + fileName;
+          let msg = "Upload successfully";
+          let funcNum = req.query.CKEditorFuncNum;
+          console.log({ url, msg, funcNum });
+
+          const script =
+            "<script>window.parent.CKEDITOR.tools.callFunction('" +
+            funcNum +
+            "','" +
+            url +
+            "','" +
+            msg +
+            "');</script>";
+
+          res.status(201).send(script);
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+router.post("/post", async (req, res) => {
+  const { title, content } = req.body;
+
+  var post = {
+    title: title,
+    body: content,
+    dateCreate: Date.now(),
+    isPending: true,
+    email: req.session.email,
+  };
+  await PostModel.create(post);
 });
 
 router.get("/report", checkStudentSession, function (req, res, next) {
