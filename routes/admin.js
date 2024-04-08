5b76ab9b3f4945609e4bd4e523eb0e0c73b025ee
Bui Duc Canh <buiduccanh10@gmail.com>
2024-04-08 16:48:19 +0700

    front-end: complete admin

--- routes/admin.js
+++ routes/admin.js
@@ -10,13 +10,11 @@ var PostModel = require("../model/post");
 router.get("/home", async function (req, res, next) {
   const deadline = await DeadlineModel.findOne({}).lean();
   const totalStudents = await StudentModel.countDocuments();
-  const totalPosts = await PostModel.countDocuments();
   res.render("admin/home", {
     layout: "admin_layout",
     admin: req.session.email,
     deadline: deadline,
     totalStudents: totalStudents,
-    totalPosts: totalPosts,
   });
 });
 
